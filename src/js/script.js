a99440ddcc5c76654396581cb23f3e8f43656a92
Manel Ariapala <amanelaa@yahoo.com>
2023-05-09 14:36:44 +0200

    Add error detention functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 const search = document.getElementById('search');
 const searchButton = document.getElementById('searchButton');
+const error = document.getElementById('error');
 const icon = "http://openweathermap.org/img/wn/";
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=57090738bfcdb9093b1d877d7903c22f&q=';
 
@@ -8,11 +9,13 @@ const countryNames = new Intl.DisplayNames(
     ['en'], { type: 'region' }
 );
 
+// API call to retrieve data
 async function weatherAPI(city) {
     const response = await fetch(apiURL + city);
 
     if (response.status === 404) {
-        document.getElementById('error').innerHTML = 'error';
+        error.style.display = 'block';
+        error.innerHTML = 'Invalid city name entered!';
     } else {
         let data = await response.json();
         console.log(data);
@@ -25,6 +28,7 @@ async function weatherAPI(city) {
         document.getElementById('windSpeed').innerHTML = data.wind.speed + ' km/h';
         document.getElementById('lowTemperature').innerHTML = Math.floor(data.main.temp_min) + ' °C';
         document.getElementById('highTemperature').innerHTML = Math.floor(data.main.temp_max) + ' °C';
+        error.style.display = 'none';
     }
 }
 
