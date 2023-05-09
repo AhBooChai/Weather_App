4b394518167421f059915c070b4b80e69612ba4e
Manel Ariapala <amanelaa@yahoo.com>
2023-05-09 14:14:03 +0200

    Complete API call to fetch data

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,11 @@ const searchButton = document.getElementById('searchButton');
 const icon = "http://openweathermap.org/img/wn/";
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=57090738bfcdb9093b1d877d7903c22f&q=';
 
+// Convert country code to country name
+const countryNames = new Intl.DisplayNames(
+    ['en'], { type: 'region' }
+);
+
 async function weatherAPI(city) {
     const response = await fetch(apiURL + city);
 
@@ -10,8 +15,16 @@ async function weatherAPI(city) {
         document.getElementById('error').innerHTML = 'error';
     } else {
         let data = await response.json();
-        document.getElementById('city').innerHTML = data.name;
+        console.log(data);
+        let country = countryNames.of(data.sys.country);
+        document.getElementById('city').innerHTML = data.name + ', ' + country;
         document.getElementById('icon').src = icon + data.weather[0].icon + '.png';
+        document.getElementById('temperature').innerHTML = Math.floor(data.main.temp) + ' °C';
+        document.getElementById('description').innerHTML = data.weather[0].description;
+        document.getElementById('humidity').innerHTML = data.main.humidity + ' %';
+        document.getElementById('windSpeed').innerHTML = data.wind.speed + ' km/h';
+        document.getElementById('lowTemperature').innerHTML = Math.floor(data.main.temp_min) + ' °C';
+        document.getElementById('highTemperature').innerHTML = Math.floor(data.main.temp_max) + ' °C';
     }
 }
 
